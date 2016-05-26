6b940b03b6a5c6c33f24e91802fc93fde0b2d8d5
littlehug <b03902081@csie.ntu.edu.tw>
2016-05-26 22:04:13 +0800

    DEBUG: interact

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # Update the apt-get first
 echo "Begin to do \"apt-get update\" to make sure you can install scipy..."
-read -p "Want to update? [Y/n]" -n1 key
-
-if [ "${key}" == "n"] || [ "${key}" == "N" ]; then
-	
-elif ["${key}" == "y"] || [ "${key}" == "Y"]; then
+read -p "Want to update? [Y/n] " -n1 key
+echo ""
+if [ "${key}" == "n" ] || [ "${key}" == "N" ]; then
+	echo "Nothing to update"
+elif [ "${key}" == "y" ] || [ "${key}" == "Y" ]; then
 	echo "sudo apt-get update"
 	sudo apt-get update
 else
@@ -15,15 +15,25 @@ else
 fi
 
 # Basic python model
+echo ""
+if [$? == 1]; then
+	exit 0
+fi
+
 echo "sudo apt-get install python-pip python-numpy python-scipy"
 sudo apt-get install python-pip python-numpy python-scipy
 
 # Upgrade numpy version
+echo ""
 if [ $? == 1 ]; then
-	exit
+	exit 0
 fi
+
 echo "pip --version"
-read -p "is your pip version 2.7? [Y/n]" key
+pip --version
+read -p "Please see above, is your pip version 2.7? [Y/n] " -n1 key
+echo ""
+
 [ "${key}" == "y" -o "${key}" == "Y" ] && echo "sudo pip install numpy --upgrade" && sudo pip install numpy --upgrade && echo "Finish!"
 [ "${key}" == "n" -o "${key}" == "N" ] && echo "please install pip as 2.7 version" && exit 0
 
